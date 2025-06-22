e177b4ea3493190ccfcac80e7c2214ceb8590020
anandadr5 <anandadwir898@gmail.com>
2025-06-22 15:44:10 +0700

    add get data & submit form for h12

---
+++ api/h12/get-data.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+
+const GAS_URL =
+  "https://script.google.com/macros/s/AKfycbw-NdGFDa4o5crGJKd_qqG3iCBJo4jqJSfzsdw06EIgV-8wIYO8XdtXFKMtjh8MLpRl/exec";
+
+const cors = require("cors")({
+  origin: "*",
+  methods: ["GET", "POST", "OPTIONS"],
+});
+
+module.exports = (req, res) => {
+  cors(req, res, async () => {
+    if (req.method !== "GET") {
+      return res.status(405).json({ error: "Method Not Allowed" });
+    }
+
+    try {
+      const query = req.query;
+      const url = new URL(GAS_URL);
+      Object.keys(query).forEach((key) =>
+        url.searchParams.append(key, query[key])
+      );
+
+      const response = await axios.get(url.toString());
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.error("Error get-data dari GAS:", error.message);
+      res.status(500).json({
+        status: "error",
+        message: "Gagal ambil data dari GAS",
+      });
+    }
+  });
+};
