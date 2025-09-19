abea37f99c816d86be834e6630f9598626cd895e
anandadr5 <anandadwir898@gmail.com>
2025-09-19 09:12:15 +0700

    update

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,6 @@
 const { sendEmail } = require("../lib/emailService");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader("Access-Control-Allow-Origin", "https://script.google.com");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
@@ -42,4 +42,4 @@ export default async function handler(req, res) {
       error: error.message,
     });
   }
-}
+};
