836ea46ba2dc3b2dc09b6eaf1089eabf2135df92
anandadr5 <anandadwir898@gmail.com>
2025-09-24 09:34:17 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sendEmailHandler = require("./api/send-email.js");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "15mb" }));
 
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
