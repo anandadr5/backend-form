78109693b32db3026aa7eecbcf27dd274d90f49d
WildanFadillah1512 <wildan.fadillah@nusaputra.ac.id>
2026-05-22 10:35:47 +0700

    PIC MULTIPLE

--- api/form.js
+++ api/form.js
@@ -10,7 +10,7 @@ const cors = require("cors")({
 
 const GAS_URLS = {
   "input-pic":
-    "https://script.google.com/macros/s/AKfycbzMWfroqPvtZXA1gz5VqdUzJhtyV_q8hWH92gl7JqFct5_dTVI2mcwmDHY6Rac5vmu-ww/exec",
+    "https://script.google.com/macros/s/AKfycbx-kTIQKrhM3Nhs827R2R2yOYJHcEMthZrLXfNGt67v4fmxGdtMPmGwTYNu7f76g6aKoQ/exec",
   login:
     "https://script.google.com/macros/s/AKfycbzCWExZ5r__w0viXeC1o5FXerwsqaC8y5XZg_W8zPMozlnLILHOJ1pPT4N-JDOFN6Jy/exec",
   h2: "https://script.google.com/macros/s/AKfycbyHaiwKENoWsOEEgj2KHr3LQW-PwfkF-Fob7fgvUV52AusSAWaY8etSmeSZeiotK7Jvhw/exec",
